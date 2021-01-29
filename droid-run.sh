4a7e85941f5203f66af036dbbea2d16f4fc2bcea
1 <a1@1s-MacBook.local>
2021-01-29 11:35:05 +0700

    refine

--- droid-run.sh
+++ droid-run.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
-rm build/app/outputs/flutter-apk/app-release.apk
-./scripts/apk-build.sh
-./scripts/apk-install.sh
+#!/bin/bash
+apkFile=build/app/outputs/flutter-apk/app-release.apk
+if [ -e $apkFile ]
+then
+	./scripts/apk-install.sh
+else
+	./scripts/apk-build.sh
+	./scripts/apk-install.sh
+fi
+rm $apk
