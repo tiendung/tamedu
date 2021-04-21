772240ca6c139ae54277019726c54dff495a2ebb
Alex <dungtn@gmail.com>
2021-04-21 15:16:03 +0700

    more teachings

--- scripts/teachings_to_opus.rb
+++ scripts/teachings_to_opus.rb
@@ -1,7 +1,7 @@
-txt = File.open("android/app/src/main/java/dev/tiendung/tamedu/Reminder.kt").read
-txt = txt.split("private val TEACHINGS = arrayOf(")[1]
+txt = File.open("android/app/src/main/java/dev/tiendung/tamedu/ReminderData.kt").read
+txt = txt.split("val TEACHINGS = arrayOf(")[1]
 txt = txt.split(")")[0]
-teachings = txt.split(/",\s*\n\s*"/).map{ |x| x.sub(/^\s*"\s*|\s*"\s*$/,"").strip }
+teachings = txt.gsub(/\/\/.+?\n/,"").split(/",\s*\n\s*"/).map{ |x| x.sub(/^\s*"\s*|\s*"\s*$/,"").strip }
 
 puts teachings
 require "uri"
@@ -9,7 +9,7 @@ require "uri"
 teachings.each_with_index do  |q, i|
 	name = "assets/teachings/#{i}"
   	next if File.exist?("#{name}.ogg") && File.size("#{name}.ogg") > 0
-	unless File.exist?("#{name}.mp3") && File.size("#{name}.mp3") > 0	 	
+	unless File.exist?("#{name}.mp3") && File.size("#{name}.mp3") > 0
 		url = "https://support.readaloud.app/read-aloud/speak/vi/GoogleTranslate%20Vietnamese?q="+URI.escape(q, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
 		puts url, name
 		`curl -o #{name}.mp3 #{url}`
