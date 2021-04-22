baa590e1c99811edd8d2158a6a8f17278de83b5e
Alex <dungtn@gmail.com>
2021-04-22 12:50:03 +0700

    bitrate 13 opus

--- scripts/teachings_to_opus.rb
+++ scripts/teachings_to_opus.rb
@@ -6,20 +6,23 @@ teachings = txt.gsub(/\/\/.+?\n/,"").split(/",\s*\n?\s*"/).map{ |x| x.sub(/^\s*"
 
 require "uri"
 # `brew install sox opus-tools`
+v = false
+# v = "Những người thành đạt trong một sự nghiệp, ở thời điểm vinh quang, thường tự giăng bẫy chính mình bằng cách cho rằng mình đã phát hiện ra một chân lý hay bí quyết nào đó đúng trong mọi trường hợp."
 teachings.each_with_index do  |q, i|
-	# next if i < 1300
 	name = "assets/#{audio_dir}/#{i}"
-  	next if File.exist?("#{name}.ogg") && File.size("#{name}.ogg") > 0
-	next if  File.exist?("#{name}.mp3") && File.size("#{name}.mp3") > 0
-	t = q; q = t.gsub(/\(.+?\)/){ |m| m.length > 15 ? "- #{m[1...-1]} -" : "" }
-	puts "#{t} => " if (t != q)
-	url = "https://support.readaloud.app/read-aloud/speak/vi/GoogleTranslate%20Vietnamese?q="+
-		URI.escape(q, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-	puts q, name
-	`curl -o #{name}.mp3 #{url}`	
-	`sox #{name}.mp3 -t s16 --rate 44100 -c 1 - | opusenc --downmix-mono --bitrate 12 --raw-rate 44100 --raw-bits 16 --raw-chan 1 - #{name}.ogg` if File.exist?("#{name}.mp3") && File.size("#{name}.mp3") > 0
+	next if v && q != v
+  	next if !v && File.exist?("#{name}.ogg") && File.size("#{name}.ogg") > 0
+	unless File.exist?("#{name}.mp3") && File.size("#{name}.mp3") > 0
+		t = q; q = t.gsub(/\(.+?\)/){ |m| m.length > 15 ? "- #{m[1...-1]} -" : "" }
+		puts "#{t} => " if (t != q)
+		url = "https://support.readaloud.app/read-aloud/speak/vi/GoogleTranslate%20Vietnamese?q="+
+			URI.escape(q, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+		puts q, name
+		`curl -o #{name}.mp3 #{url}`
+	end
+	`sox #{name}.mp3 -t s24 --rate 24000 -c 1 - | opusenc --downmix-mono --bitrate 13 --raw-rate 24000 --raw-bits 24 --raw-chan 1 - #{name}.ogg` if File.exist?("#{name}.mp3") && File.size("#{name}.mp3") > 0
 end
 end
 
 # tto("val TEACHINGS = arrayOf(", "teachings")
-x = tto("val QUOTES_BY_LEN_DESC = arrayOf(", "quotes")
+tto("val QUOTES_BY_LEN_DESC = arrayOf(", "quotes")
